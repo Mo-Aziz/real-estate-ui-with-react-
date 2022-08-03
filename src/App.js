6b40f8e3ae7d5ddc6479d8c0d3ec71cad919f3c6
Mo-Aziz <mohamed.aref32@gmail.com>
2022-08-03 04:26:21 -0500

    Finished styling and desiging the footer, added some fixes for the featured section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "../src/components/navbar/Navbar"
 import Hero from  "../src/components/hero/Hero"
 import Best from "../src/components/best/Best"
 import Featured from './components/featured/Featured';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Hero/>
     <Best/>
     <Featured/>
+    <Footer/>
     </>  
   
   );
